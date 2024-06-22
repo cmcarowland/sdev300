82068499fef56700a9da784696063c21b23ec29a
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-06-22 11:43:44 +0000

    Added Tests and ran Lab
    test: Added tests for input, sorting and finding elements
    chore: Ran lab for video

--- Lab3/src/lab3_tests.py
+++ Lab3/src/lab3_tests.py
@@ -54,3 +54,56 @@ def test_set_state_population():
     test_cases = [[100, True], [-1, False],[-100, False],[200,True], [600000, True]]
     for test in test_cases:
         assert sorted_states[0].set_population(test[0]) is test[1]
+
+
+def test_int_inputs(monkeypatch):
+    """Tests the function get_int_entry for valid response"""
+    test_cases = {'31':31, '0':0, '18':18, '120':120, '17':17, '121':121, 'asdf':None,'':None}
+    for case, result in test_cases.items():
+        monkeypatch.setattr('builtins.input', lambda _: case)
+        x = lab3.get_int_entry('')
+        assert x == result
+
+def test_string_inputs(monkeypatch):
+    """Tests the function get_string_entry for valid response"""
+    test_cases = ['Bob', 'Larry', '  ', '    Chuck   ', 'Curly', 'Mo']
+    for case in test_cases:
+        monkeypatch.setattr('builtins.input', lambda _: case)
+        x = lab3.get_string_entry('')
+        assert x == case
+
+def test_bool_inputs(monkeypatch):
+    """Tests the function get_yes_no for valid response"""
+    test_cases = {'y':True, 'yes':True, 'YES':True, 'n':False, 'N':False, 'no':False, 'No':False, \
+                  'asdf':None, 'Hi':None}
+    for case, result in test_cases.items():
+        monkeypatch.setattr('builtins.input', lambda _: case)
+        x = lab3.get_yes_no('')
+        assert x == result
+
+def test_is_null_or_whitespace():
+    """Tests the is_null_or_whitespace function"""
+    test_cases = {"hi":False, "   Hi":False, "Hi.   ":False, "   Hi.  ":False,\
+                  "     ":True, None:True, '':True}
+
+    for case, result in test_cases.items():
+        x = lab3.is_null_or_whitespace(case)
+        assert x == result
+
+def test_find_first():
+    """Tests the find first function"""
+    test_cases = [["MD","Maryland", True],
+                  ["asdf", None, False],
+                  ["Vermont", "Vermont", True],
+                  ["200",None,False],
+                  ["600000",None, False],
+                  ["", None, False]]
+    for case in test_cases:
+        print(case)
+        state_name = case[0]
+        s = lab3.find_first(x for x in states \
+                if x.name == state_name or x.abbreviation == state_name.upper())
+
+        assert isinstance(s, lab3.State) == case[2]
+        if s is not None:
+            assert s.name == case[1]
